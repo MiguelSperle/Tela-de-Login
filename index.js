5c5b18092cecfb5961c43f74ab64fe27a47748f2
Miguel <102910354+MiguelSperle@users.noreply.github.com>
2022-11-24 14:46:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let email = document.querySelector('.input-email')
+let password = document.querySelector('.input-password')
+let submit = document.querySelector('.input-submit')
+
+submit.addEventListener('click', () => {
+    if(email.value == ''){
+        email.setCustomValidity('Coloque seu Email')
+        email.style.border = '1px solid red'
+        email.style.outline = '1px solid red'
+    } else {
+        email.setCustomValidity('')
+        email.style.border = '1px solid black'
+        email.style.outline = '1px solid black'
+    }
+
+
+    if(password.value == ''){
+        password.setCustomValidity('Coloque sua Senha')
+        password.style.border = '1px solid red'
+        password.style.outline = '1px solid red'
+    } else {
+        password.setCustomValidity('')
+        password.style.border = '1px solid black'
+        password.style.outline = '1px solid black'
+        
+    }
+})
+
+
+
+
